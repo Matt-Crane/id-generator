27c8b046794d2d2c19d4819a789475199f9e5b93
Matt-Crane <matt.crane@bristol.ac.uk>
2021-09-28 10:59:00 +0100

    LLC id-generator v1
    Altered encryption, automated where possible, auto gen salt, loop encryption on duplication...
    
    Todo, clean up further and add UK LLC specific automation

--- deterministic_id_generator.py
+++ deterministic_id_generator.py
@@ -3,78 +3,153 @@ import luhn
 import hashlib as hl
 import os
 import sys
-#import tkinter as tk
-#import pyodbc
+from tkinter import *
+from tkinter import filedialog as fd
+import random
+import string
 
 
-# STEP 1: FILE SETUP
-# change dir to to test output dir, can be chnaged to suit
-wd = input('specify working directory for outputs... ')
-os.chdir(wd)
-# import file - chnaged to ODBC to SQL to get lastest ALF from file 1- will need updating if file 1 changed
-#cnxn = pyodbc.connect('DSN=?????')
-#query = 'SELECT * FROM schema.table_name;'
-#idbase = pd.read_sql(query, cnxn)
-inputfile = input('specify file for input including full filepath and file extension... ')
-# read in file as per user defined input
-idbase = pd.read_csv(inputfile)
-# select and keep column to hash - user can select one column from list 
-idtohash = input('please type name of id column to hash option in input file are' +str(list(idbase))+' ... ')
-idbase = idbase[[idtohash]]
-
-# STEP 2: HASHING TO GENERATE CONSISTENT ID
-# add salt 
-salt = input('define a salt... ')
-idbase['idplussalt'] = idbase[idtohash].apply(lambda x: str(x) + salt)
-# perform SHA256 hash
-idbase['hashedid'] = [hl.sha256(str.encode(str(i))).hexdigest() for i in idbase['idplussalt']]
-# remove chars - only want digits - does run risk of introducing dups - this checked a few lines doen
-chars = {'a':'','b':'','c':'','d':'','e':'','f':''}
-idbase['hashedid2'] = idbase['hashedid'].replace(chars, regex=True)
-# string down to first 9 digits
-idbase['hashedid9'] = idbase['hashedid2'].str[:9]
-# dulicate checker
-idbase['dupflag'] = idbase.duplicated(['hashedid9'], keep=False)
-# check all ids are unqiue any will return true if one or more elements are ture. If false, all ids are unique
-if idbase.dupflag.any() == True:
-    print('STOP - duplicate IDs. Please rerun and define new salt')
-    sys.exit()
-    #print('STOP - duplicate IDs. Please define new salt')
-elif idbase.dupflag.all() == False:
-    print('no duplication in ids with defined salt')
-# create directory for salt
-if not os.path.exists(wd+'/salt'):
-    os.makedirs(wd+'/salt') 
-# store salt for reproducability
-text_file = open(wd+'\salt\\'+'salt'+salt[:5]+'.txt','w+')
-text_file.write(salt)
-text_file.close()
-
-# STEP 3: add prefix
-# define 2 digit prefix for id - should evaluate if len is 2
-prefix = input('Define a 1 digit prefix number for id ')
-idbase['hashedid11'] = str(prefix) + idbase['hashedid9']
-
-# STEP 4
-# generate the luhn check digit in new column for visability (will be dropped)
-idbase['luhn1'] = idbase['hashedid11'].apply(lambda x: luhn.generate(x))
- # append the check digit
-idbase['hashedid12'] = idbase['hashedid11'].apply(lambda x: luhn.append(x))
-# test verify
-idbase['luhnvalid'] = idbase['hashedid12'].apply(lambda x: luhn.verify(x))
-# check if any invalid numbers
-if idbase.luhnvalid.all() == True:
-    print('no issue with check digits')
-elif idbase.luhnvalid.all() == False:
-    sys.exit('STOP - issue with luhn check digit')
+def file_dialog():
+    '''
+    Opens file dialog to find appropriate files
+    '''
+    root = Tk()
+    root.withdraw()
     
-# STEP 5: cleanup and strip down
-# rename new id column
-idcolname = input('specify column name for new ID... ')
-idbase.rename(columns={'hashedid12':idcolname},inplace=True)
-# strip down file
-sailpreg = idbase[[idtohash, idcolname]]
-# create input for file name
-filename = input('please define a filename for output... ')
-# export as csv
-sailpreg.to_csv(filename+'.csv', index=False)
+    filetypes = (
+        ('text files', '*.txt'),
+        ('csv files', '*.csv')
+    )
+    
+    filename = fd.askopenfilename(
+        title='Open a file',
+        initialdir=os.getcwd(),
+        filetypes=filetypes)
+    return filename
+
+
+def generate_salt(n = 20):
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=n))
+
+
+def input_settings():
+    '''
+    Load the args and set the program settings
+    '''
+    if len(sys.argv) > 1 and sys.argv[1].lower() == "true":
+        try:
+            LLC_mode = True
+        except:
+            print("Error in input 'LLC_MODE' (position 1), using default settings")
+
+        if len(sys.argv) > 2:
+            try: 
+                N = int(sys.argv[2])
+            except EXCEPTION:
+                print("Error in input 'N' (position 2), using default value")
+        else:
+            N = 9
+    else:
+        LLC_mode = False
+
+        
+    return LLC_mode, N
+
+if __name__ == "__main__":
+
+    # Removing a irritating window that pops up by default with tkinter
+    root = Tk()
+    root.withdraw()
+
+    # Get input settings (LLC_mode True/False) for program flow
+    LLC_mode, N = input_settings()
+
+    # STEP 1: FILE SETUP
+    # change dir to to test output dir, can be changed to suit
+    wd = fd.askdirectory()
+    os.chdir(wd)
+    # import file - changed to ODBC to SQL to get lastest ALF from file 1- will need updating if file 1 changed
+    #cnxn = pyodbc.connect('DSN=?????')
+    #query = 'SELECT * FROM schema.table_name;'
+    temp_schema_placeholder = "SCHEMA"
+    #idbase = pd.read_sql(query, cnxn)
+    inputfile = file_dialog()
+    # read in file as per user defined input
+    idbase = pd.read_csv(inputfile)
+    # select and keep column to hash - user can select one column from list 
+    if LLC_mode:
+        idtohash = "ALF_E"
+    else:
+        idtohash = input('please type name of id column to hash option in input file are' +str(list(idbase))+' ... ')
+    idbase = idbase[[idtohash]]
+
+    # STEP 2: HASHING TO GENERATE CONSISTENT ID
+    # add salt 
+
+    while True:
+        salt = generate_salt()
+        idbase['idplussalt'] = idbase[idtohash].apply(lambda x: str(x) + salt)
+        # perform SHA256 hash and remove chars by converting to decimal. 
+        idbase['hashedid'] = [str(int(hl.sha256(str.encode(str(i))).hexdigest(), 16))for i in idbase['idplussalt']]
+
+        # string down to first 9 digits. Increases risk of duplicates
+        idbase['hashedidN'] = idbase['hashedid'].str[:N]
+        # dulicate checker
+        idbase['dupflag'] = idbase.duplicated(['hashedidN'], keep=False)
+        # check all ids are unqiue any will return true if one or more elements are ture. If false, all ids are unique
+        print(idbase)
+        if idbase.dupflag.any() == True:
+            print('Duplicate IDs. Regenerating salt...')
+            continue
+            # restart loop
+        else:
+            break
+            # Once on duplicates are generated, continue
+    # create directory for salt
+    salt_path = os.path.join(wd,'salt')
+    if not os.path.exists(salt_path):
+        os.makedirs(salt_path) 
+    else:
+        raise FileExistsError("File {} already exists".format(salt_path))
+    # store salt for reproducability
+    text_file = open(os.path.join(salt_path, 'salt'+temp_schema_placeholder+'.txt'),'w')
+    text_file.write(salt)
+    text_file.close()
+
+    # STEP 3: add prefix
+    # define 2 digit prefix for id - should evaluate if len is 2
+    if LLC_mode:
+        prefix = "1"
+    else:
+        prefix = input('Define a 1 digit prefix number for id ')
+    idbase['hashedidN_pref'] = str(prefix) + idbase['hashedidN']
+
+    # STEP 4
+    # generate the luhn check digit in new column for visability (will be dropped)
+    idbase['luhn1'] = idbase['hashedidN_pref'].apply(lambda x: luhn.generate(x))
+    # append the check digit
+    idbase['hashedidN_pref_check'] = idbase['hashedidN_pref'].apply(lambda x: luhn.append(x))
+    # test verify
+    idbase['luhnvalid'] = idbase['hashedidN_pref_check'].apply(lambda x: luhn.verify(x))
+    # check if any invalid numbers
+    if idbase.luhnvalid.all() == True:
+        print('check digits passed validation')
+    elif idbase.luhnvalid.all() == False:
+        raise ValueError("Luln check digit failed to validate")
+        
+    # STEP 5: cleanup and strip down
+    # rename new id column
+    if LLC_mode:
+        idcolname = "encrytpedID"
+    else:
+        idcolname = input('specify column name for new ID... ')
+    idbase.rename(columns={'hashedidN_pref_check':idcolname},inplace=True)
+    # strip down file
+    sailpreg = idbase[[idtohash, idcolname]]
+    # create input for file name
+    if LLC_mode:
+        filename = os.path.split(inputfile)[1].split(".")[0] + "_encypted"
+    else:
+        filename = input('please define a filename for output... ')
+    # export as csv
+    sailpreg.to_csv(filename+'.csv', index=False)
